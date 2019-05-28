c6d8bf2dbbdeac6dd0656b7a324513ec1026e83f
Akash Bisht <akashbisht81@gmail.com>
2019-05-28 10:35:20 +0530

    Alma mater !

--- tf_assign1.py
+++ tf_assign1.py
@@ -10,6 +10,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 import tensorflow as tf
+#Alma mater !
 
 sess = tf.InteractiveSession()
 ###############################################################################
